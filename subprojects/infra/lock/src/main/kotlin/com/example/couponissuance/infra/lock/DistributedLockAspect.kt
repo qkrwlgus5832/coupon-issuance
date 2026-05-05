10e27c868dbe415f8f3cf3adcd80894064baeb1e
qkrwlgus5832 <pjh_5890@naver.com>
2026-05-05 19:42:11 +0900

    redis cluster 설정하기

--- subprojects/infra/lock/src/main/kotlin/com/example/couponissuance/infra/lock/DistributedLockAspect.kt
+++ subprojects/infra/lock/src/main/kotlin/com/example/couponissuance/infra/lock/DistributedLockAspect.kt
@@ -5,12 +5,14 @@ import org.aspectj.lang.ProceedingJoinPoint
 import org.aspectj.lang.annotation.Around
 import org.aspectj.lang.annotation.Aspect
 import org.aspectj.lang.reflect.MethodSignature
+import org.springframework.core.annotation.Order
 import org.springframework.expression.spel.standard.SpelExpressionParser
 import org.springframework.expression.spel.support.StandardEvaluationContext
 import org.springframework.stereotype.Component
 
 @Aspect
 @Component
+@Order(1)
 class DistributedLockAspect(
     private val redisService: RedisService,
 ) {
--- subprojects/infra/redis/src/main/kotlin/com/example/couponissuance/infra/redis/config/RedisConfig.kt
+++ subprojects/infra/redis/src/main/kotlin/com/example/couponissuance/infra/redis/config/RedisConfig.kt
@@ -3,22 +3,30 @@ package com.example.couponissuance.infra.redis.config
 import org.redisson.Redisson
 import org.redisson.api.RedissonClient
 import org.redisson.config.Config
+import org.redisson.config.ReadMode
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 class RedisConfig(
-    @Value("\${spring.data.redis.host}") private val host: String,
-    @Value("\${spring.data.redis.port}") private val port: Int,
+    @Value("\${spring.data.redis.cluster.nodes}") private val nodes: String,
 ) {
 
-    @Bean
+    @Bean(destroyMethod = "shutdown")
     fun redissonClient(): RedissonClient {
         val config = Config()
-        config.useSingleServer().apply {
-            address = "redis://127.0.0.1:$port"
+        val clusterConfig = config.useClusterServers()
+            .setReadMode(ReadMode.MASTER_SLAVE)
+            .setScanInterval(2000)
+            .setRetryAttempts(3)
+            .setRetryInterval(1500)
+            .setTimeout(3000)
+
+        nodes.split(",").forEach { node ->
+            clusterConfig.addNodeAddress("redis://${node.trim()}")
         }
+
         return Redisson.create(config)
     }
 }
